294756992cc9aac6cc3f212768d031bade04decc
thiarly <thiarly.cavalcante@live.com>
2023-05-15 16:17:03 -0300

    create base

--- SiteComunidade/main.py
+++ SiteComunidade/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
+
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+

5bdc76d582d8ce7a5a84040e2600ffb1d61c5672
thiarly <thiarly.cavalcante@live.com>
2023-05-16 10:59:45 -0300

    create navbar

--- SiteComunidade/main.py
+++ SiteComunidade/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuario = ['Thiarly', 'Luca', 'Laila', 'Clara']
+
 @app.route("/")
 def hello_world():
     return render_template('home.html')
@@ -10,6 +12,11 @@ def hello_world():
 @app.route("/contato")
 def contato():
     return render_template('contato.html')
+
+
+@app.route("/usuarios")
+def usuario():
+    return render_template('usuarios.html', lista_usuario=lista_usuario)
 
 if __name__ == '__main__':
     app.run(debug=True)

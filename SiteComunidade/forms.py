f611e138188ada7855c8b25824ae936fee514fd6
thiarly <thiarly.cavalcante@live.com>
2023-05-17 15:11:17 -0300

    SiteComunidade.py
    Update lembrar dados - lembrar e adicionando

--- SiteComunidade/forms.py
+++ SiteComunidade/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
@@ -7,10 +7,11 @@ class FormCriarConta(FlaskForm):
     email = StringField('Email', validators=[Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=20)])
     confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
-    botao_sumit_criarconta = SubmitField('Criar Conta')
+    botao_submit_criarconta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=20)])
-    botao_sumit_login = SubmitField('Fazer Login')
+    lembrar_dados = BooleanField('Lembrar')
+    botao_submit_login = SubmitField('Fazer Login')

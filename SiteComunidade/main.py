91d5536643c7406fe61775edefa5ebcd55e6be4d
thiarly <thiarly.cavalcante@live.com>
2023-05-19 17:10:32 -0300

    criando mensagem
    criando mensagem de alerta ao fazer login e criar conta

--- SiteComunidade/main.py
+++ SiteComunidade/main.py
@@ -25,11 +25,11 @@ def login():
     form_login = FormLogin()  # Instância do formulário FormLogin
     form_criarconta = FormCriarConta()  # Instância do formulário FormCriarConta
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash('Login feito com sucesso no e-mail: {}'.format(form_login.email.data))
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
         
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-         flash('Conta criada para o e-mail: {}'.format(form_criarconta.email.data))
+         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
          return redirect(url_for('home'))
 
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)

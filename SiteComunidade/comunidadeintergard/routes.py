e5e1567b9bbf369e37b4f118e49c9717dcd1edfb
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-05-28 07:55:56 -0300

    Update routes.py
    criando variável foto perfil para pegar a foto automáticamente

--- SiteComunidade/comunidadeintergard/routes.py
+++ SiteComunidade/comunidadeintergard/routes.py
@@ -57,7 +57,8 @@ def sair():
 @app.route('/perfil')
 @login_required
 def perfil():
-    return render_template('perfil.html')
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
 
 @app.route('/post/criar')
 @login_required

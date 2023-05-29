4b2ae9e3cf74b53668aeda06e8da656768e4aeb0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-05-29 15:39:00 -0300

    Update routes.py

--- SiteComunidade/comunidadeintergard/routes.py
+++ SiteComunidade/comunidadeintergard/routes.py
@@ -3,6 +3,9 @@ from comunidadeintergard.forms import FormCriarConta, FormLogin, FormEditarPerfi
 from comunidadeintergard.models import Post, Usuario
 from comunidadeintergard import app, database, bcrypt
 from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image
 
 lista_usuario = ['Thiarly', 'Luca', 'Laila', 'Clara']
 
@@ -65,6 +68,16 @@ def perfil():
 def criar_post():
     return render_template('criarpost.html')
 
+def salvar_imagem(imagem):
+    codigo = secrets.token_hex(6)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
+    tamanho = (200, 200)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+    imagem_reduzida.save(caminho_completo)
+    return nome_arquivo
 
 @app.route('/editar/perfil', methods=["GET", "POST"])
 @login_required
@@ -73,6 +86,10 @@ def editar_perfil():
     if form.validate_on_submit():
         current_user.email = form.email.data
         current_user.username = form.username.data
+        if form.foto_perfil.data:
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+            
         database.session.commit()
         flash('Perfil atualizado com sucesso', 'alert-success')
         return redirect(url_for('perfil'))

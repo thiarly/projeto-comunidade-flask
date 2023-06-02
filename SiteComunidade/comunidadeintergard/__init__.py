5b66c51dda4d665b645f3fc7cae7a17d20aa4903
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-02 15:27:09 -0300

    Update __init__.py

--- SiteComunidade/comunidadeintergard/__init__.py
+++ SiteComunidade/comunidadeintergard/__init__.py
@@ -2,11 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6e9142134c8c3b306eef0ce3c1d5d585'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)

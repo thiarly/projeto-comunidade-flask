26e023ad40691b5dbdf0944b452cf3c52d4fd44e
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-20 14:22:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ with app.app_context():
     database.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
       
